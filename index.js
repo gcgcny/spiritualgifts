f36e1b62fbed85384359a9a8e193186e453570fb
Simeon Wong <dtxe@users.noreply.github.com>
2023-01-16 13:18:23 -0500

    autoscroll, save progress

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// identify which version of the quiz to use based on hash
+let VERSION = window.location.hash.substring(1);
+if (VERSION != 'adult' && VERSION != 'youth') {
+    VERSION = 'adult';
+}
 
 // question component
 // const question_component = (question, category) => `
@@ -24,7 +29,7 @@
 //       </div>`;
 
 const question_component = (question, category, index) => {
-    let radiobuttons = Array(10).fill(1).map((_, i) => `<input type="radio" class="btn-check" name="question${index}" autocomplete="off" value="${i}" id="c${index}-${i}" data-category="${category}"><label class="btn btn-outline-secondary btn-sm" for="c${index}-${i}">${i+1}</label>`).join('');
+    let radiobuttons = Array(10).fill(1).map((_, i) => `<input type="radio" class="btn-check" name="question${index}" autocomplete="off" value="${i}" id="c${index}-${i}" data-category="${category}"><label class="btn btn-outline-secondary btn-sm" for="c${index}-${i}">${i + 1}</label>`).join('');
 
     return `<div class="card my-4">
         <div class="card-body">
@@ -40,7 +45,7 @@ const question_component = (question, category, index) => {
           </div>
         </div>
       </div>`
-    };
+};
 
 const progress_component = (width, text) => `
 <div class="progress my-2" style="height: 1.5rem;">
@@ -52,24 +57,10 @@ const progress_component = (width, text) => `
     <div class="justify-content-left align-self-center ps-4 d-flex position-absolute w-100 fs-6 fw-medium">${text}</div>
 </div>`;
 
-
-// identify which version of the quiz to use based on hash
-let version = window.location.hash.substring(1);
-if(version != 'adult' && version != 'youth') {
-    version = 'adult';
-}
-
-// loop through questions and write to screen
-let html_questions = SURVEY[version].map((q, i) => {
-    return question_component(q.question, q.category, i);
-}).join('');
-document.getElementById('qcontainer').innerHTML = html_questions;
-
-
 // score quiz
 const score_quiz = () => {
     // get all the question types
-    let categories = SURVEY[version].map((q) => q.category);
+    let categories = SURVEY[VERSION].map((q) => q.category);
 
     // get unique categories
     categories = [...new Set(categories)];
@@ -110,7 +101,63 @@ const score_quiz = () => {
     // remove width
     html_scores = html_scores.map((s) => s[1]).join('');
 
-    document.getElementById('results').innerHTML = html_scores;
+    document.getElementById('results').innerHTML = `<h3 class="mt-5 mb-4">Your personal spiritual gifts inventory</h3>` + html_scores;
 };
 
+
+// loop through questions and write to screen
+let html_questions = SURVEY[VERSION].map((q, i) => {
+    return question_component(q.question, q.category, i);
+}).join('');
+document.getElementById('qcontainer').innerHTML = html_questions;
+
+
+// bind submit button
 document.getElementById('btnSubmit').addEventListener('click', score_quiz);
+
+
+// check if there's an in-progress version already, and restore it
+let progress = localStorage.getItem('progress-' + VERSION);
+
+if (progress) {
+    progress = JSON.parse(progress);
+
+    document.querySelectorAll('.btn-check').forEach((el) => {
+        el.checked = false;
+    });
+
+    // loop through the progress and set the values
+    for (let sel of progress) {
+        console.log(sel);
+        document.getElementById(sel).checked = true;
+    }
+}
+
+
+// bind change event to all the questions
+document.querySelectorAll('.btn-check').forEach((el) => {
+    el.addEventListener('change', (e) => {
+        // scroll to next question
+        let next = el.closest('.card').nextElementSibling;
+        if (next) {
+            next.scrollIntoView({ behavior: 'smooth', block: 'center' });
+        }
+
+        // save progress
+        let progress = Array.from(document.querySelectorAll('.btn-check:checked')).map((el) => el.id);
+
+        localStorage.setItem('progress-'
+            + VERSION, JSON.stringify(progress));
+    });
+});
+
+
+// bind clear event
+document.getElementById('btnClear').addEventListener('click', (e) => {
+    localStorage.removeItem('progress-' + VERSION);
+
+    document.querySelectorAll('.btn-check').forEach((el) => {
+        el.checked = false;
+    });
+    document.getElementById('results').innerText = "";
+});
